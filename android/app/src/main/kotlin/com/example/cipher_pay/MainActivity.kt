1df12576349293a6b71a3a75b8f51eee86d05a0c
Shashank <99321679+Ayushh1023@users.noreply.github.com>
2023-04-07 03:59:48 +0530

    newInterface

--- android/app/src/main/kotlin/com/example/cipher_pay/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cipher_pay
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,27 @@
-import 'package:cipher_pay/pages/createuser.dart';
-import 'package:cipher_pay/pages/first.dart';
-import 'package:cipher_pay/pages/login.dart';
-import 'package:flutter/material.dart';
-import 'package:cipher_pay/utils/routes.dart';
-import 'package:cipher_pay/pages/meta_login_page.dart';
+// ignore_for_file: prefer_const_constructors
 
-void main(List<String> args) {
-  runApp(const MyApp());
+import 'package:crypto_part/pages/home.dart';
+import 'package:crypto_part/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: MyRoutes.firstpage,
-      routes: {
-        MyRoutes.mloginRoute: (context) => const MLoginPage(),
-        MyRoutes.firstpage: (context) => FirstPage(),
-        MyRoutes.login: (context) => Login(),
-        MyRoutes.create: (context) => CreateUser(),
-      },
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: primary,
+        brightness: Brightness.dark,
+      ),
+      home: Home(),
     );
   }
 }
