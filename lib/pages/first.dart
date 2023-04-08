23f3df0810faad4a9ca029c9bafde2b259c0847f
Vaibhav yadav <Vaibhav160404@gmail.com>
2023-04-08 21:53:38 +0530

    Vaibhav Commit

--- lib/pages/first.dart
+++ lib/pages/first.dart
@@ -3,14 +3,23 @@ import 'package:crypto_part/pages/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../theme/colors.dart';
+
 class First extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [appBgColorPrimary, appBgColorSecondary],
+        ),
+      ),
       child:Column(
         children: [
           SizedBox(height: 100,),
-          const Text("Go to your CHOICE Transactions"),
+
           SizedBox(height: 100,),
           ElevatedButton(onPressed:() {
             Navigator.pushNamed(context,MyRoutes.gotocryto);
