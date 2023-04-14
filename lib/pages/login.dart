a2b64d1459ac96dbdd25fa5ff75c5719ded64240
Vaibhav yadav <Vaibhav160404@gmail.com>
2023-04-15 01:41:05 +0530

    Login Credentials Added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class Login extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
     return  Material(
--- lib/pages/wrapper.dart
+++ lib/pages/wrapper.dart
@@ -30,3 +30,5 @@ class Wrapper extends StatelessWidget {
     );
   }
 }
+
+
