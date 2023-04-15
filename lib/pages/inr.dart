c22cfc2487adbba0e61b5ab2b16a45f45bfd79fe
Vaibhav yadav <Vaibhav160404@gmail.com>
2023-04-15 18:19:27 +0530

    Native data Added

--- lib/pages/inr.dart
+++ lib/pages/inr.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_element
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,21 +17,20 @@ class _INRState extends State<INR> {
 
   void _depositAmount(double amount) {
     setState(() {
-      balance += amount;
+      balance+=amount;
       depositController.text = '';
     });
   }
-
   void _withdrawAmount(double amount) {
     setState(() {
-      balance -= amount;
+      balance-=amount;
       withdrawController.text = '';
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    var balance = 0;
     return Scaffold(
       appBar: AppBar(
         title: Text("Native Payments"),
@@ -55,38 +52,31 @@ class _INRState extends State<INR> {
                 ),
                 TextFormField(
                   controller: withdrawController,
-                  decoration: const InputDecoration(hintText: "Amount in INR", labelText: "Enter Withdraw Amount"),
+                  decoration: const InputDecoration(
+                      hintText: "1000.00", labelText: "Enter Withdraw Amount"),
                   keyboardType: TextInputType.number,
                   style: const TextStyle(fontSize: 15),
-
-
                 ),
-                ElevatedButton(onPressed: () {}, child: Text("Withdraw")),
+                ElevatedButton(onPressed: () {
+                  double amount = double.parse(withdrawController.text);
+                  _withdrawAmount(amount);
+                }, child: Text("Withdraw")),
                 TextFormField(
                   controller: depositController,
                   decoration: const InputDecoration(
-                      hintText: "Amount in INR", labelText: "Enter Deposit Amount"),
+                      hintText: "1000.00", labelText: "Enter Deposit Amount"),
                   style: const TextStyle(fontSize: 15),
                   keyboardType: TextInputType.number,
                 ),
-                ElevatedButton(
-                    onPressed: () {
-                      double amount = double.parse(depositController.text);
-                      _depositAmount(amount);
-                    },
-                    child: Text("Deposit"))
+                ElevatedButton(onPressed: () {
+                  double amount = double.parse(depositController.text);
+                  _depositAmount(amount);
+                }, child: Text("Deposit"))
               ],
             ),
           ),
         ),
       ),
     );
-
-  }
-  int deposit(int balance, int deposit){
-    return balance+deposit;
-  }
-  int withdraw(int balance, int withdraw){
-    return balance-withdraw;
   }
 }
